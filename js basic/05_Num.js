2fe5fe971273b6ffd12db476b2f844e4fab0a813
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-03-20 10:13:29 +0530

    num done

---
+++ js basic/05_Num.js
@@ -0,0 +1,60 @@
+let score = 10
+console.log(score)
+
+const balance = new Number(100) // Js Number Object use imp- use for console and learn its various functions
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))//precision value is 2
+console.log("what is Needed For Happiness");
+
+let numbnew = 23.89898;
+
+console.log(numbnew.toString())
+console.log(numbnew.toPrecision(3))//shift the decimal
+console.log(numbnew.toPrecision(4));
+
+const hundreds = 100000000000; 
+
+console.log(hundreds.toLocaleString()) // , added in the no Output: 1,00,00,00,00,000 
+console.log(hundreds.toLocaleString('en-IN'));// for use of Indian numb Values
+
+//++++++++++++++ Math Javascript ++++++++++++++++++
+
+let positiveNum = 5;
+let negativeNum = -10;
+let zero = 0;
+
+console.log(Math.abs(positiveNum)); // Output: 5
+console.log(Math.abs(negativeNum)); // Output: 10
+console.log(Math.abs(zero)); // Output: 0
+
+
+// console.log(Math);//output: Object [Math] {}
+// // console.log(math.abs(-4)); // used to find the absolute value
+// console.log(math.round(4,6));
+// console.log(math.ceil(4.9));//choose top value
+// console.log(math.floor(3.8))//choose lower value
+
+// console.log(math.min(4,3,6,7));//find mim-value
+// console.log(math.max(4,3,6,8));//find max-value
+// console.log(math.randon());//find random Number
+
+
+// ______________Practice_______________\\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
