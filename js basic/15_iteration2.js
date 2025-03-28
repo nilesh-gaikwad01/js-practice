2905df039acc6c0dabbfeea7be98f51805d00802
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-03-28 17:11:10 +0530

    practice

---
+++ js basic/15_iteration2.js
@@ -0,0 +1,21 @@
+// High Order Array Loop
+
+
+// ["","",""],
+// [{},{},{}]
+
+// for of loop
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "hello, world"
+
+for (const greet of greetings) {
+    console.log(`Each Char is $(greetings)`);
+    
+    
+}
