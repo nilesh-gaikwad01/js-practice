56787179f1a5c4eb2d91bde79047a0d3619dad57
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-04-01 23:54:57 +0530

    dom

---
+++ js basic/test1.js
@@ -0,0 +1,31 @@
+// destruction of an object
+
+const person = {
+    name : "Nilesh",
+    sirname : "Gaikwad",
+    age :25,
+    position:"Developer"
+}
+
+//How to Destructure this object , simply the destructring means the the objetct properties assign to the variable directely call  them in console instead of usng the object name.
+
+let {name, sirname, position} = person 
+
+console.log(name);
+console.log(sirname);
+// console.log(age); nordestruct
+console.log(position);
+
+person.name = "web-developer"
+person.position = "development+Devops"
+
+console.log(name);
+console.log(position);
+
+person.age =  "Pune, Mumbai, Bangaluru"
+const {age:city} = person
+
+console.log(city)
+ 
+
+//
