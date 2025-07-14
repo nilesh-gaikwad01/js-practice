48156f917c6a864e8af46206277a94d7af00cde1
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-07-14 23:02:53 +0530

    second commit

--- js basic/05_Num.js
+++ js basic/05_Num.js
@@ -4,15 +4,15 @@ console.log(score)
 const balance = new Number(100) // Js Number Object use imp- use for console and learn its various functions
 console.log(balance);
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))//precision value is 2
-console.log("what is Needed For Happiness");
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))//precision value is 2
+// console.log("what is Needed For Happiness");
 
 let numbnew = 23.89898;
 
-console.log(numbnew.toString())
-console.log(numbnew.toPrecision(3))//shift the decimal
-console.log(numbnew.toPrecision(4));
+// console.log(numbnew.toString())
+// console.log(numbnew.toPrecision(3))//shift the decimal
+// console.log(numbnew.toPrecision(4));
 
 const hundreds = 100000000000; 
 
@@ -25,9 +25,9 @@ let positiveNum = 5;
 let negativeNum = -10;
 let zero = 0;
 
-console.log(Math.abs(positiveNum)); // Output: 5
-console.log(Math.abs(negativeNum)); // Output: 10
-console.log(Math.abs(zero)); // Output: 0
+// console.log(Math.abs(positiveNum)); // Output: 5
+// console.log(Math.abs(negativeNum)); // Output: 10
+// console.log(Math.abs(zero)); // Output: 0
 
 
 // console.log(Math);//output: Object [Math] {}
