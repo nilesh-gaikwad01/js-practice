6ab3e6d7054d00e3edb6eb5a5242b8e2172762db
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-07-24 19:26:24 +0530

    oops

---
+++ js basic/oppsConcept/notes.md
@@ -0,0 +1,18 @@
+
+## OOP
+
+## Object
+-collection of properties and methods
+
+##Parts of OOP
+
+-Object literal
+-Prototypes
+-Classes
+-Instances (new, this)
+
+## Pillers of OOP
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ js basic/oppsConcept/oop.js
@@ -0,0 +1,44 @@
+// object literal this keyword and current, global exectution context
+
+const user = {
+    username : "Ravi Gupta",
+    loginCount : 8,
+
+    getUserDetails : function (){
+        // console.log("here is the user details");
+        console.log(this.username);
+        
+        // console.log(this) // it return the currecnt context
+        
+    }
+}
+
+console.log(user.username);
+console.log(user.getUserDetails());
+
+// console.log(this); // It return the empty object {}, in global context
+
+
+// Constructor Function
+
+// new keyword - 
+ // new keyword create new instance for the object.
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username,loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    // return this // implicitely define alreay not necessary to return
+}
+
+const userOne = new User("ravi Gupta",12,true)
+const userTwo =  new User("Hitesh Chowdhari",18,false)
+
+
+// console.log(userOne) // Values Overrights
+console.log(userTwo) // new keywords create its new instance , then value can't overrights
+
+console.log(userOne.constructor);
