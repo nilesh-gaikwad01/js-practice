af4c87621c1b047bba689b267d9691ebc6586bbc
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2025-07-25 23:48:38 +0530

    one

---
+++ js basic/asyns/one.js
@@ -0,0 +1,24 @@
+
+
+// asyn code in javascript
+
+//1. Javascript default behaviour 
+
+// javascrtipt is a single Threaded and syncronus language 
+// it excute code one by one sometimes it is asynchronus but mostley it work in syncronus
+
+// 2. Javascript execution context
+
+//javascript engine is single threaded
+//call stack and memory heap 
+
+
+// 3.Blocking code and Non blocking code
+
+// 4.JS V8 Enging
+
+//Call stack and Memory Heap
+
+// web API
+
+
